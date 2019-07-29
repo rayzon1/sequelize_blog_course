f224a1b187fad47219389232f4cea9e037ce692f
Gerardo Keys <jotojry@icloud.com>
2019-07-28 21:46:24 -0700

    initial commit for project, work completed up to fourth video

---
+++ models/article.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Article = sequelize.define('Article', {
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    body: DataTypes.TEXT
+  }, {});
+  Article.associate = function(models) {
+    // associations can be defined here
+  };
+  return Article;
+};
