5d6c20deccc8fabe841848e73a8284ccd88a4e65
Gerardo Keys <jotojry@icloud.com>
2019-07-29 17:52:24 -0700

    completed up to 5th video

--- models/article.js
+++ models/article.js
@@ -1,12 +1,21 @@
 'use strict';
+
+var dateFormat = require('dateformat');
+
 module.exports = (sequelize, DataTypes) => {
   const Article = sequelize.define('Article', {
     title: DataTypes.STRING,
     author: DataTypes.STRING,
     body: DataTypes.TEXT
-  }, {});
-  Article.associate = function(models) {
-    // associations can be defined here
-  };
+  });
+  
+  Article.prototype.publishedAt = function() {
+    return dateFormat(this.createdAt, "dddd, mmmm dS, yyyy, h:MM TT");
+  }
+  
+  Article.prototype.shortDescription = function() {
+    return this.body.length > 30 ? this.body.substr(0, 30) + "..." : this.body;
+  }
+
   return Article;
 };
